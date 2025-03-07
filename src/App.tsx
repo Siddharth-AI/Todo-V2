95c179ed373999b889a5af2323b64004708acc77
Siddharth Shah <sidshah895@gmail.com>
2025-03-07 19:56:28 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { AddToDo } from "./components/AddToDo";
+
+export const App = () => {
+  return (
+    <main>
+      <h1>TODO REACT + TYPESCRIPT</h1>
+      <AddToDo />
+    </main>
+  );
+};
